e34cea699c40f5c52d9362c3905ef8cfa63dd4aa
Preston Windfeldt <prestonwindfeldt@gmail.com>
2024-02-20 17:02:01 -0700

    trigger action working and composer local

--- client/app/dashboard/composer/bundled-composer/api/client.js
+++ client/app/dashboard/composer/bundled-composer/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3000";
+const baseURL = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3002";
 
 const apiClient = axios.create({
     baseURL: `${baseURL}/api/v1`,
