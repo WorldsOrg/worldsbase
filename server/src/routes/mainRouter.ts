be9cc77a76c66469844e5606c713b15e17f9d9d3
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2024-02-26 11:10:14 +0300

    remove unused routes

--- server/src/routes/mainRouter.ts
+++ server/src/routes/mainRouter.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
 import tableRoutes from "./tableRoutes";
-import walletRoutes from "./walletRoutes";
-import onChainRoutes from "./onChainRoutes";
 import dashboardRoutes from "./dashboardRoutes";
 import authRoutes from "./authRoutes";
 
@@ -9,8 +7,6 @@ const mainRouter = Router();
 
 mainRouter.use("/auth", authRoutes);
 mainRouter.use("/table", tableRoutes);
-mainRouter.use("/wallet", walletRoutes);
-mainRouter.use("/onChain", onChainRoutes);
 mainRouter.use("/dashboard", dashboardRoutes);
 
 export default mainRouter;
