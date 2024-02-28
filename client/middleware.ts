7c207b662f12622b00888c058651b3da37e09759
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2024-02-28 15:13:37 +0300

    add middleware console to proxy

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,15 +3,19 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  console.log(request.nextUrl.pathname);
   if (request.nextUrl.pathname.startsWith("/api")) {
+    console.log(process.env.API_PATH, request.url);
     return NextResponse.rewrite(new URL(process.env.API_PATH as string, request.url));
   }
 
   if (request.nextUrl.pathname.startsWith("/composerapi")) {
+    console.log(process.env.COMPOSER_API_PATH, request.url);
     return NextResponse.rewrite(new URL(process.env.COMPOSER_API_PATH as string, request.url));
   }
 
   if (request.nextUrl.pathname.startsWith("/chainapi")) {
+    console.log(process.env.CHAIN_API_PATH, request.url);
     return NextResponse.rewrite(new URL(process.env.CHAIN_API_PATH as string, request.url));
   }
 }
