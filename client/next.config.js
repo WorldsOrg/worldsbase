5dcddb77f8bb8906ca4f7968316b678cba5b05ea
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2024-02-28 13:25:29 +0300

    fix nextjs warning sharp

--- client/next.config.js
+++ client/next.config.js
@@ -4,15 +4,15 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_PATH}/:path*`,
+        destination: `${process.env.API_PATH}/:path*`,
       },
       {
         source: "/composerapi/:path*",
-        destination: `${process.env.NEXT_PUBLIC_COMPOSER_API_PATH}/:path*`,
+        destination: `${process.env.COMPOSER_API_PATH}/:path*`,
       },
       {
         source: "/chainapi/:path*",
-        destination: `${process.env.NEXT_PUBLIC_CHAIN_API_PATH}/:path*`,
+        destination: `${process.env.CHAIN_API_PATH}/:path*`,
       },
     ];
   },
