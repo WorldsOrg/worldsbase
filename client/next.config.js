10d012e00a9c785f5f5171b24cf86dccd485761a
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2024-02-28 13:52:17 +0300

    fix nextconfig rewrites

--- client/next.config.js
+++ client/next.config.js
@@ -4,15 +4,15 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_PATH}/:path*`,
+        destination: process.env.API_PATH ? `${process.env.API_PATH}/:path*` : "http://localhost:3003/api/:path*",
       },
       {
         source: "/composerapi/:path*",
-        destination: `${process.env.COMPOSER_API_PATH}/:path*`,
+        destination: process.env.COMPOSER_API_PATH ? `${process.env.COMPOSER_API_PATH}/:path*` : "http://localhost:3008/composerapi/:path*",
       },
       {
         source: "/chainapi/:path*",
-        destination: `${process.env.CHAIN_API_PATH}/:path*`,
+        destination: process.env.CHAIN_API_PATH ? `${process.env.CHAIN_API_PATH}/:path*` : "http://localhost:3004/chainapi/:path*",
       },
     ];
   },
