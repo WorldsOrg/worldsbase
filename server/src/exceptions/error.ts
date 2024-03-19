93f52a0e2fdee6c1a6081567c262030f7b0a65a5
Ozgur Sallanci <ozgur.sallanci@gmail.com>
2024-03-19 22:58:06 +0300

    remove landing and dashboard page

--- server/src/exceptions/error.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-import { CustomError } from "../types";
-
-export const handleErrors = (error: unknown, res: Response) => {
-  const customError = error as CustomError;
-  res.status(customError.status ?? 500).json(customError);
-};
--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-// src/index.ts
-
-import app from "./app";
-
-const PORT = process.env.PORT || 3003;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
